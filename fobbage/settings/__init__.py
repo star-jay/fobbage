114250ddac63d95e01b493646fb9e7a5e6713bb2
44410016 <reinjan.e@gmail.com>
2019-01-25 18:13:19 +0100

    pick any password you want

--- fobbage/settings/__init__.py
+++ fobbage/settings/__init__.py
@@ -119,22 +119,7 @@ AUTH_USER_MODEL = 'accounts.User'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation'
-                '.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.'
-                'NumericPasswordValidator',
-    },
+
 ]
 
 
