7330d8ff40c3f701241a8dd6af6ccee3ab783081
44410016 <reinjan.e@gmail.com>
2019-07-11 17:12:11 +0200

    share sebsocket link in serializeer

--- fobbage/quizes/api/serializers.py
+++ fobbage/quizes/api/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from django.urls import reverse as django_reverse
+from rest_framework.reverse import reverse
+
 from fobbage.quizes.models import (
     Quiz, Question, Bluff, Answer, Guess
 )
@@ -72,6 +75,30 @@ class QuestionSerializer(serializers.ModelSerializer):
 class QuizSerializer(serializers.ModelSerializer):
 
     active_question = serializers.SerializerMethodField()
+    websocket = serializers.SerializerMethodField()
+
+    def get_websocket(self, instance):
+        request = self.context.get('request', None)
+        if request.scheme == 'http':
+            scheme = 'ws'
+        else:
+            scheme = 'wss'
+
+        location = django_reverse(
+            'room',
+            args=(instance.id, ),
+        )
+        # location = django_reverse(
+        #      'room',
+        #     args=(instance.id, ),
+        #     request=request,
+        # )
+
+        return '{}://{}/ws{}'.format(
+            scheme,
+            request.get_host(),
+            location,
+        )
 
     def get_active_question(self, quiz):
         # active_round = quiz.rounds.get(is_active=True)
@@ -88,4 +115,5 @@ class QuizSerializer(serializers.ModelSerializer):
             'id',
             'title',
             'active_question',
+            'websocket',
         )
