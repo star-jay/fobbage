19ea289b93f9fa448d9006536626447d5c474bad
44410016 <reinjan.e@gmail.com>
2020-07-01 21:27:39 +0200

    serve dist folder

--- fobbage/settings/__init__.py
+++ fobbage/settings/__init__.py
@@ -174,7 +174,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'dist'),
 )
 
 # Simplified static file serving.
