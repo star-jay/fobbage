7c63b8d3e078732e6cb6b6d4ac90f1e352e546a9
44410016 <reinjan.e@gmail.com>
2020-07-09 18:22:42 +0200

    use fobbageClient

--- src/services/api/bluffs.js
+++ src/services/api/bluffs.js
@@ -1,3 +1,4 @@
 import Resource from '@/services/resource';
+import client from './fobbageClient';
 
-export default new Resource('api/bluffs');
+export default new Resource(client, 'api/bluffs');
--- src/services/api/quizes.js
+++ src/services/api/quizes.js
@@ -1,3 +1,4 @@
 import Resource from '@/services/resource';
+import client from './fobbageClient';
 
-export default new Resource('api/quizes');
+export default new Resource(client, 'api/quizes');
