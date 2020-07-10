73b6c654ced86496be45ba59e187c4df6dc44a97
44410016 <reinjan.e@gmail.com>
2020-07-10 17:01:40 +0200

    cleanup API

--- fobbage/accounts/urls.py
+++ fobbage/accounts/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import url
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 from rest_framework.authtoken.views import obtain_auth_token
-from . import views
 
 urlpatterns = [
     url(r'^simple_token/$',
