f097529db8f26ab89fc6d5b5fc6e8dc39096657a
Reinjan <reinjan.e@gmail.com>
2022-12-05 22:18:29 +0100

    Update packages and solve django 4 related stuff

--- fobbage/accounts/urls.py
+++ fobbage/accounts/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import UserAPIView
 
 urlpatterns = [
-    url(r'user_info/$', UserAPIView.as_view(), name='user-info'),
-    url(r'^simple_token/$',
-        obtain_auth_token,
-        name='simple-token'),
+    path(r'user_info/$', UserAPIView.as_view(), name='user-info'),
+    path(r'^simple_token/$', obtain_auth_token, name='simple-token'),
 ]
